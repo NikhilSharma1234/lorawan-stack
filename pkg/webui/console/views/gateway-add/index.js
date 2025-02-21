e1fe0de504475e862c3f262b3e651208812588bd
Kaden <kadennesch@gmail.com>
2025-02-21 12:01:30 -0800

    console: Added help button element for registering Gateway&Sensor

--- pkg/webui/console/views/gateway-add/index.js
+++ pkg/webui/console/views/gateway-add/index.js
@@ -12,13 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React, { useCallback } from 'react'
+import React, { useCallback, useState} from 'react'
 import { Container, Col, Row } from 'react-grid-system'
 import { defineMessages } from 'react-intl'
 import { useNavigate } from 'react-router-dom'
 
 import PageTitle from '@ttn-lw/components/page-title'
 import Link from '@ttn-lw/components/link'
+
+import { Dialog, DialogContent, DialogTitle, IconButton, Box } from '@mui/material'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import videoFile from '@assets/videos/Map.mp4'
 
 import Message from '@ttn-lw/lib/components/message'
 import RequireRequest from '@ttn-lw/lib/components/require-request'
@@ -46,6 +50,7 @@ const GatewayGuideLink = content => (
 
 const GatewayAdd = () => {
   const navigate = useNavigate()
+  const [openVideo, setOpenVideo] = useState(false)
   const handleSuccess = useCallback(
     (gtwId, isManaged = false) => {
       if (isManaged) {
@@ -61,6 +66,24 @@ const GatewayAdd = () => {
     <Require featureCheck={mayCreateGateways} otherwise={{ redirect: '/gateways' }}>
       <RequireRequest requestAction={getOrganizationsList()}>
         <Container>
+          <div style={{ display: 'flex', justifyContent: 'flex-end'}}>
+            <Box
+              sx={{ boxShadow: 4 }}
+              onClick={() => setOpenVideo(true)}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                cursor: 'pointer',
+                padding:'0px 8px',
+                borderRadius: '8px',
+              }}
+            >
+              <IconButton>
+                <HelpOutlineIcon style={{ fontSize: '26px' }} />
+              </IconButton>
+              <p>Help Video</p>
+            </Box>
+          </div>
           <PageTitle
             colProps={{ md: 10, lg: 9 }}
             className="mb-cs-s"
@@ -78,6 +101,25 @@ const GatewayAdd = () => {
               <GatewayOnboardingForm onSuccess={handleSuccess} />
             </Col>
           </Row>
+          <Dialog
+            open={openVideo}
+            onClose={() => setOpenVideo(false)}
+            maxWidth="md"
+            style={{ zIndex: '2001' }}
+            PaperProps={{
+              style: {
+                borderRadius: '6px',
+              },
+            }}
+          >
+            <DialogTitle style={{ alignSelf: 'center' }}>Map Video Guide</DialogTitle>
+            <DialogContent>
+              <video controls style={{ width: '100%' }}>
+                <source src={videoFile} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </DialogContent>
+          </Dialog>
         </Container>
       </RequireRequest>
     </Require>
